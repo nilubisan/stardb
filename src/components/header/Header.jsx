07a4a6cc8f1af695d2f205934926272f03c69122
nilubisan <r.nas9329@gmail.com>
2022-07-05 00:47:08 +0600

    feat: add MUI, styles for header and main page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,128 @@
 import React from "react";
-
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import Logo from './logo.png';
 import "./header.css";
 
-const Header = () => {
-    return (
-        <div className="header d-flex">
-            <h3>
-                <a href="#">
-                    Star DB
-                </a>
-            </h3>
-            <ul className="d-flex">
-                <li>
-                    <a href="#">People</a>
-                </li>
-                <li>
-                    <a href="#">Planets</a>
-                </li>
-                <li>
-                    <a href="#">Starships</a>
-                </li>
-            </ul>
-        </div>
-    );
-};
+const ResponsiveAppBar = () => {
 
-export default Header;
+const pages = ['People', 'Planets', 'Starships'];
+
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+        <Box
+            component="img"
+            sx={{
+            height: 64,
+            }}
+            src={Logo}
+        />
+          <Typography
+            variant="h4"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'Arial',
+              fontWeight: 900,
+              letterSpacing: '.1rem',
+              color: '#FFDD00',
+              textDecoration: 'none',
+            }}
+          >
+            StarDB
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography color='#FFDD00' textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: '#FFDD00',
+              textDecoration: 'none',
+            }}
+          >
+            StarDB
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: '#FFDD00', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default ResponsiveAppBar;
